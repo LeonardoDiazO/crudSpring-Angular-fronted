b49be0c9c508ad4e9361b777c13656d3fa938623
LeonardoDiazO <maximun_neo@hotmail.com>
2024-10-24 20:36:57 -0500

    feat: btnNew, routeBookService and renderizacion de html para cuando carguen los libros

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-
+import { Book } from '../models/book';
+import { BookService } from '../services/book.service';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, CardModule],
+  imports: [ButtonModule, CardModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  books:Book[] = [];
+
+  constructor(private bookService: BookService){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+
+    
+  }
+
+  getAllBooks(){
+    this.bookService.getBook().subscribe((data) =>{
+      this.books = data;
+    });
+  }
 
 }
