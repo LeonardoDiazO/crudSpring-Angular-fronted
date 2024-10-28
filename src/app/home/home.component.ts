bb1517d2d5d7f2f579bff0cabdb134c17f654ed0
LeonardoDiazO <maximun_neo@hotmail.com>
2024-10-27 19:44:39 -0500

    feat: add event btn edit & delete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,25 +4,30 @@ import { CardModule } from 'primeng/card';
 import { Book } from '../models/book';
 import { BookService } from '../services/book.service';
 import { RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, CardModule, RouterModule],
+  imports: [ButtonModule, CardModule, RouterModule, ToastModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
   books:Book[] = [];
+  isDeleteInProgress:boolean = false;
 
-  constructor(private bookService: BookService){}
+  constructor(private bookService: BookService,
+              private messageService: MessageService
+  ){}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
+    this.getAllBooks();
 
     
   }
@@ -32,5 +37,32 @@ export class HomeComponent {
       this.books = data;
     });
   }
+
+  deleteBook(id:number){
+
+    this.isDeleteInProgress = true;
+    this.bookService.deleteBook(id).subscribe({
+      next:()=>{
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Correcto',
+          detail: 'Libro eliminado ',
+        });
+        this.isDeleteInProgress = true;
+        this.getAllBooks();
+      },
+      error:()=>{
+        this.isDeleteInProgress = true;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudo eliminar el libro',
+        });
+      }
+
+
+    })
+  }
+
 
 }
